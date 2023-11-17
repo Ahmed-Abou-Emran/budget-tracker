f70f65c8959a013db842433f33a3624c510e5f86
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-11-17 20:53:57 +0200

    updating assets

---
+++ src/assets/index.jsx
@@ -0,0 +1,39 @@
+import Avatar from "./avatar.jpg";
+import Debts from "./debts.png";
+import Food from "./food.png";
+import Health from "./health.png";
+import Logo from "./logo.svg";
+import Others from "./others.png";
+import Rent from "./rent.png";
+import Savings from "./savings.png";
+import Subscription from "./subscription.png";
+import User from "./user.svg";
+import Entertainment from "./entertainment.png";
+import Cart from "./cart.svg";
+
+export {
+  Avatar,
+  Debts,
+  Food,
+  Health,
+  Logo,
+  Others,
+  Rent,
+  Savings,
+  Subscription,
+  Entertainment,
+  User,
+  Cart,
+};
+
+// avatar.png;
+// debts.png;
+// food.png;
+// health.png;
+// logo.svg;
+// others.png;
+// rent.png;
+// savings.png;
+// subscription.png;
+// user.svg;
+// entertainment.png;
